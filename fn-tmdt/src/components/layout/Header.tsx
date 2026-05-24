e4898b78792f879c75a189e4298711412a4db286
blueneko <blueneko@hizozima.id.vn>
2026-05-24 22:55:16 +0700

    add something

--- fn-tmdt/src/components/layout/Header.tsx
+++ fn-tmdt/src/components/layout/Header.tsx
@@ -5,12 +5,16 @@ export const Header: React.FC = () => {
   return (
     <header className="w-full sticky top-0 z-50 bg-surface shadow-sm flex items-center justify-between px-8 py-4 font-headline antialiased">
       <div className="flex items-center gap-8">
-        <span className="text-2xl font-bold tracking-tight text-on-surface">Atélier</span>
+        <a href="/" className="hidden md:flex items-center gap-4">
+          <img src="/assets/img/logo.png" alt="Logo" className="w-12 h-12 img-pixelated" />
+          <span className="text-2xl font-bold tracking-tight text-on-surface">Atélier</span>
+        </a>
+
         <div className="hidden md:flex items-center bg-surface-container-low px-4 py-2 rounded-full w-96">
           <Search className="text-on-surface-variant mr-2" size={20} />
-          <input 
-            className="bg-transparent border-none focus:ring-0 text-sm w-full text-on-surface placeholder-on-surface-variant/60" 
-            placeholder="Tìm kiếm tài nguyên..." 
+          <input
+            className="bg-transparent border-none focus:ring-0 text-sm w-full text-on-surface placeholder-on-surface-variant/60"
+            placeholder="Tìm kiếm tài nguyên..."
             type="text"
           />
         </div>
@@ -23,9 +27,9 @@ export const Header: React.FC = () => {
             <Bell className="text-on-surface-variant" size={24} />
           </button>
           <div className="w-10 h-10 rounded-full overflow-hidden border-2 border-surface-container-high scale-102 transition-transform duration-300 ease-out">
-            <img 
-              alt="Hồ sơ người dùng" 
-              className="w-full h-full object-cover" 
+            <img
+              alt="Hồ sơ người dùng"
+              className="w-full h-full object-cover"
               src="https://lh3.googleusercontent.com/aida-public/AB6AXuCgjenSZvmWQAHLKLu_fM4v9SZRZ5DPygvM4mF4-TlaoifBbIrtbx7MeVFpDB2CgAiBf6Olwz4ICP4Plwu_0wfr6r-N-ww-oFnsUeOMkWFCZ156VXAI6-zoqaMlPVQ1cxNZRWipMYMI9hvlB-aSrTz6cDJdt8v8cD9ZCeRcDBa5cSyGFLiMuffdtl8AbMa4HtDhvfdC3NwkAjOw2DAoum7ndBfvubfjy3t0mN2xPJZoRVeP1VS6yiFtETW8eVGimEItoeJTxP4ONZFz"
             />
           </div>
